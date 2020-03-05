f401576b8ebc35f2b89d83764819550dc09f8378
Corentin Hatte <contact@corentin-hatte.eu>
2020-03-05 18:09:55 +0100

    Redesign welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,123 +7,49 @@
 		<title>{{ config('app.name') }}</title>
 
 		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
 		<!-- Styles -->
-		<style>
-			html, body {
-				background-color: #fff;
-				color: #242729;
-				font-family: 'Nunito', sans-serif;
-				font-weight: 200;
-				height: 100vh;
-				margin: 0;
-				background-size: cover;
-				background-position: center;
-			}
-
-			.full-height {
-				height: 100vh;
-				background-color: rgba(255, 255, 255, .1);
-			}
-
-			.flex-center {
-				align-items: center;
-				display: flex;
-				justify-content: center;
-			}
-
-			.position-ref {
-				position: relative;
-			}
-
-			.top-right {
-				position: absolute;
-				top: 0;
-				right: 0;
-				width: 100%;
-				padding: 0;
-				display: flex;
-				justify-content: right;
-				background-color: #fff;
-				box-shadow: 0 0 4px rgba(31, 31, 31, .5);
-			}
-
-			.content {
-				text-align: center;
-			}
-
-			.title {
-				font-size: 84px;
-				text-shadow: 0 0 5px rgba(255, 255, 255, .7);
-			}
-
-			.links > a {
-				display: inline-block;
-				color: inherit;
-				padding: 18px 25px;
-				font-size: 13px;
-				font-weight: 600;
-				letter-spacing: .1rem;
-				text-decoration: none;
-				text-transform: uppercase;
-			}
-
-			.m-b-md {
-				margin-bottom: 30px;
-			}
-
-
-			@media (prefers-color-scheme: dark) {
-				html, body {
-					background-color: #212529;
-					color: #f8fafc;
-				}
-				.navbar {
-					background-color: black !important;
-					color: white;
-				}
-				.navbar a {
-					color: inherit;
-				}
-				.full-height {
-					background-color: rgba(0, 0, 0, .1);
-				}
-				.top-right {
-					background-color: #000;
-				}
-				.title {
-					font-size: 84px;
-					text-shadow: 0 0 5px rgba(0, 0, 0, .7);
-				}
-			}
-		</style>
+		<link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 	</head>
-	<body style="background-image: url('/storage/splashscreens/{{ $splashscreen->file_name }}');">
-		<div class="flex-center position-ref full-height">
-			@if (Route::has('login'))
-				<div class="top-right links">
-					@auth
-						<a href="{{ route('dashboard') }}">{{ __('Tableau de bord') }}</a>
-					@else
-						<a href="{{ route('login') }}">{{ __('Connexion') }}</a>
+	<body>
+		<div class="navbar">
+			<div class="container">
+				<nav class="navbar-left">
+					<a href="" class="navbar-item navbar-logo">{{ config('app.name') }}</a>
+				</nav>
+				<nav class="navbar-right">
+				@auth
+					<a href="{{ route('dashboard') }}" class="navbar-item">{{ __('Tableau de bord') }}</a>
+				@else
+					<a href="{{ route('login') }}" class="navbar-item">{{ __('Connexion') }}</a>
+					<a href="{{ route('register') }}" class="navbar-item">{{ __('Inscription') }}</a>
+				@endauth
+				</nav>
+			</div>
+		</div>
+		<div class="splashscreen">
+			<div class="container">
+				<div class="splashscreen-content">
+					<h1 class="brand">{{ config('app.name') }}</h1>
 
-						@if (Route::has('register'))
-							<a href="{{ route('register') }}">{{ __('Inscription') }}</a>
-						@endif
-					@endauth
-				</div>
-			@endif
+					<div class="slogan">
+						<span>La plateforme pour</span>
+						<span>les associations sportives</span>
+					</div>
 
-			<div class="content">
-				<div class="title m-b-md">{{ config('app.name') }}</div>
-
-				<div class="links">{{ __('La plateforme pour les associations sportives') }}</div>
-
-				<div class="links">
-					@foreach($sports as $sport)
-					<a href="{{ route('clubs').'/'.$sport->slug }}">{{ $sport->name }}</a>@if (!$loop->last) | @endif
-					@endforeach
+					<nav class="sports-list">
+						<div class="lane">
+							@foreach($sports as $sport)
+							<a href="{{ route('clubs').'/'.$sport->slug }}">{{ $sport->name }}</a>
+								@if ($loop->iteration%6 === 0 && !$loop->last)
+						</div>
+						<div class="lane">
+								@elseif ($loop->iteration%3 === 0 && !$loop->last)
+							<br>
+								@endif
+							@endforeach
+						</div>
+					</nav>
 				</div>
 			</div>
 		</div>
